beba1ae06bb78c9572699c24288663b673f6a6bd
Amey Arun Thorat <88768050+thoratamey@users.noreply.github.com>
2022-03-19 16:18:16 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='HospitalManagementSystem',
+    version='2.7',
+    packages=['package'],
+    url='',
+    license='',
+    author='Amey',
+    author_email='thoratamey1210@gmail.com',
+    description='Hospital Management System Project'
+)
